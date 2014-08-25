fd36bf3489087f303273c552b3a9656744cdf5fd
Jack Stevenson <cogwirrel@gmail.com>
2014-08-25 11:22:25 +0100

    Highlight the contact section when you click free quote

--- js/ajk.js
+++ js/ajk.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
 	skrollr.init({
 		smoothScrolling: true
 	});
+
+	$('#quote-corner').click(function() {
+		$('.contact').removeClass('contact-no-highlight');
+		$('.contact').addClass('contact-highlight');
+
+		window.setTimeout(function() {
+			$('.contact').addClass('contact-no-highlight');
+			$('.contact').removeClass('contact-highlight');
+		}, 300);
+		
+	});
 });
