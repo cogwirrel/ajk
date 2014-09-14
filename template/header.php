d8c54e44010f3f22c567825e9ef04d9ded98acf9
Jack Stevenson <cogwirrel@gmail.com>
2014-09-15 00:19:53 +0100

    Add tagline and location to normal header because it looks nicer and is more consistent

--- template/header.php
+++ template/header.php
@@ -1,7 +1,9 @@
 <div id="home" class="row">
 	<div class="col-md-12">
 		<div class="item-container">
-			<h1 class="header"><?php echo $name_caps; ?></h1>
+			<h1 class="header"><?php echo strtoupper($name); ?></h1>
+			<h2 class="sub-header"><?php echo $tagline; ?></h2>
+			<h2 id="location-sub-header" class="sub-header"><?php echo $where; ?></h2>
 			<div class="row">
 				<div class="col-md-6">
 					<p class="contact-header"><a href="mailto:<?php echo $email; ?>"><i class="fa fa-envelope-o"></i><br><?php echo $email; ?></a></p>
