80c78761548635d9bd0f1801f2f790d23ccf82c5
Jack Stevenson <cogwirrel@gmail.com>
2014-09-15 00:20:20 +0100

    Create about us page, needs placeholders to be replaced

--- about/index.php
+++ about/index.php
@@ -6,6 +6,83 @@
 
 <?php $active='about'; include('../template/nav.php'); ?>
 
-hello
+<div class="row">
+	<div class="col-md-8">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="item-container">
+					<h1>About Us</h1>
+					<p align="justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus hendrerit iaculis. Duis sit amet massa sem. Pellentesque imperdiet risus nulla, nec commodo nulla porta vitae. Cras augue libero, ullamcorper sed quam euismod, imperdiet luctus dolor. Etiam aliquet dignissim nisl vel blandit. Mauris eu interdum diam. Etiam mattis ipsum sem, in volutpat turpis sodales id.
+						<br><br>Morbi quis rhoncus risus. Integer eget ipsum vitae nisi scelerisque tincidunt eu eleifend ex. Nam malesuada neque est, sit amet mollis ante volutpat nec. Curabitur commodo turpis in ex porta, dictum dictum nisl egestas. Quisque interdum purus ac ipsum ornare, vitae vestibulum arcu vestibulum. Mauris volutpat a mauris et cursus. Integer semper vitae dui a laoreet. Pellentesque ut ultricies tortor. Mauris eget quam lacus. Donec commodo neque a facilisis vehicula. Suspendisse vulputate nibh sit amet semper imperdiet. Curabitur ornare ullamcorper velit, sed pulvinar lectus porta nec. Sed pellentesque elit nisi, condimentum condimentum nibh maximus id. Mauris quis turpis at lacus vulputate venenatis in eget nisi. Nam eu vehicula justo, ut mattis ante. Donec non rutrum augue, vel consequat metus.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="space-60"></div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<div class="item-container">
+					<h1>Who We Work With</h1>
+
+					<div class="row">
+						<div class="col-md-6">
+							<h3>FFFC</h3>
+							<div class="charity-logo-container">
+								<img class="charity-logo-img" src="http://www.fffc.org.uk/graphics/fffc-logo.jpg"></img>
+							</div>
+							<div class="space-15"></div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus hendrerit iaculis. Duis sit amet massa sem. Pellentesque imperdiet risus nulla, nec commodo nulla porta vitae.</p>
+						</div>
+
+						<div class="col-md-6">
+							<h3>BHF</h3>
+							<div class="charity-logo-container">
+								<img class="charity-logo-img" src="http://www.incaproductions.co.uk/wp-content/uploads/2012/04/bhf-gallery.png"></img>
+							</div>
+							<div class="space-15"></div>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent finibus hendrerit iaculis. Duis sit amet massa sem. Pellentesque imperdiet risus nulla, nec commodo nulla porta vitae.</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="col-md-4">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="item-container">
+					<img class="side-img" src="http://placekitten.com/500/300"></img>
+				</div>
+			</div>
+		</div>
+		<div class="space-15"></div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="item-container">
+					<img class="side-img" src="http://placekitten.com/400/400"></img>
+				</div>
+			</div>
+		</div>
+		<div class="space-15"></div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="item-container">
+					<img class="side-img" src="http://placekitten.com/300/400"></img>
+				</div>
+			</div>
+		</div>
+		<div class="space-15"></div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="item-container">
+					<img class="side-img" src="http://placekitten.com/300/150"></img>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php include('../template/bottom.php'); ?>
